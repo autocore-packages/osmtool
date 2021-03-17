d200baf7e2c72f4cbb83e9dc1fc02b637ee5d207
BaoHan <1004578954@qq.com>
2021-03-17 14:02:43 +0800

    Init

---
+++ Scripts/Node.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AutoCore.OSM
+{
+    public class Node : OSMElement
+    {
+        public float ele;
+        public float local_x;
+        public float local_y;
+        public Node() { }
+    }
+}
---
+++ Scripts/OSMData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AutoCore.OSM
+{
+    public class OSMData
+    {
+        public List<Node> nodes;
+        public List<Way> ways;
+        public List<Relation> relations;
+        public OSMData()
+        {
+            nodes = new List<Node>();
+            ways = new List<Way>();
+            relations = new List<Relation>();
+        }
+    }
+}
